45ca34021576de67d278b712da195fa18797a3d2
ines <inesqtr@gmail.com>
2019-10-18 11:45:01 +0100

    quest 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Quote from "./Quote";
+import Lamp from "./Lamp";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { working: true };
+    this.handleClickButton = this.handleClickButton.bind(this);
+  };
+
+  // handleClickButton() {
+    // this.setState({ working: !this.state.working })
+  // };
+
+  handleClickButton() {
+    this.setState((state) => {
+      return {working: !state.working};
+    })
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={this.state.working ? "App-logo" : "App-logo-transform"} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+          <button
+            onClick={this.handleClickButton}>Working
+          </ button>
+        </header>
+
+        <Lamp on />
+        <Lamp />
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+
+      </ div>
+    );
+  }
 }
+
 
 export default App;
